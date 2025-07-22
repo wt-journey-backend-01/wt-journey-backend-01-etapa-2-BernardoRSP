8e239bfc96256b6093cf7d025dff09ebc22fc820
Bernardo Rezende <bernardo.com5@gmail.com>
2025-07-21 21:25:13 -0300

    Linhas com as rotas ainda não desenvolvidas comentada

--- routes/agentesRoutes.js
+++ routes/agentesRoutes.js
@@ -5,8 +5,8 @@ const agentesController = require("../controllers/agentesController.js");
 router.get("/agentes", agentesController.getAllAgentes);
 router.get("/agentes/:id", agentesController.getAgenteById);
 router.post("/agentes", agentesController.adicionarAgente);
-router.put("/agentes/:id", agentesController.atualizarAgente);
-router.patch("/agentes/:id", agentesController.atualizarAgenteParcial);
+//router.put("/agentes/:id", agentesController.atualizarAgente);
+//router.patch("/agentes/:id", agentesController.atualizarAgenteParcial);
 router.delete("/agentes/:id", agentesController.deleteAgenteById);
 
 module.exports = router;
--- routes/casosRoutes.js
+++ routes/casosRoutes.js
@@ -5,8 +5,8 @@ const casosController = require("../controllers/casosController.js");
 router.get("/casos", casosController.getAllCasos);
 router.get("/casos/:id", casosController.getCasoById);
 router.post("/casos", casosController.adicionarCaso);
-router.put("/casos/:id", casosController.atualizarCaso);
-router.patch("/casos/:id", casosController.atualizarCasoParcial);
+//router.put("/casos/:id", casosController.atualizarCaso);
+//router.patch("/casos/:id", casosController.atualizarCasoParcial);
 router.delete("/casos/:id", casosController.deleteCasoById);
 
 module.exports = router;
