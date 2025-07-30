a445f8044aa678c92caafba6cbae213eed2ee02e
Bernardo Rezende <bernardo.com5@gmail.com>
2025-07-29 21:06:59 -0300

    Atualização de erros 404 e 400

--- repositories/casosRepository.js
+++ repositories/casosRepository.js
@@ -16,7 +16,7 @@ function adicionar(caso) {
 function atualizar(caso, id) {
   const indice = casos.findIndex((c) => c.id === id);
   if (indice !== -1) {
-    casos[indice] = { ...caso, id }; // Garante que o id não será perdido ou alterado
+    casos[indice] = caso;
     return casos[indice];
   }
   return null;
