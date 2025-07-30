3519f362424289c72d62b4d01761dc2a5d039d7d
Bernardo Rezende <bernardo.com5@gmail.com>
2025-07-29 22:13:37 -0300

    Atualização de erros 404 e 400

--- repositories/agentesRepository.js
+++ repositories/agentesRepository.js
@@ -1,4 +1,11 @@
-const agentes = [];
+const agentes = [
+  {
+    id: "401bccf5-cf9e-489d-8412-446cd169a0f1",
+    nome: "Rommel Carneiro",
+    dataDeIncorporacao: "1992/10/04",
+    cargo: "delegado",
+  },
+];
 
 function findAll() {
   return agentes;
